055ba9cd29e76b2472457913b21df9f8f257f075
Mason Hardy <hardymasonj@gmail.com>
2021-10-09 16:12:24 -0500

    Added additional projects

---
+++ HardyStrategies.Testing.Common/Enumeration/Browsers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardyStrategies.Testing.Common.Enumeration
+{
+	public class Browsers
+	{
+		public enum BrowserType
+		{
+			Chrome,
+			Firefox,
+			Edge
+		}
+	}
+}
---
+++ HardyStrategies.Testing.SeleniumUtilities/DriverUtilities/DriverFactory.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using HardyStrategies.Testing.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardyStrategies.Testing.SeleniumUtilities.DriverUtilities
+{
+	public class DriverFactory
+	{
+		public IWebDriver GetWebDriver(Common.Enumeration.Browsers.BrowserType eBrowserType)
+		{
+			switch (eBrowserType)
+			{
+				case Common.Enumeration.Browsers.BrowserType.Chrome:
+					return new ChromeDriver(Common.Constant.TestSetup.WebDrivers.ChromeDriverLocation);
+			}
+			return null;
+		}
+	}
+}
