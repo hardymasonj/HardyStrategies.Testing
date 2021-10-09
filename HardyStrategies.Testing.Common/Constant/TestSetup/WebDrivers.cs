9f0bde3abb68d5fb479c735da5dccbe13d2b6101
Mason Hardy <hardymasonj@gmail.com>
2021-10-09 15:45:34 -0500

    Add project files.

---
+++ HardyStrategies.Testing.Common/Constant/TestSetup/WebDrivers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardyStrategies.Testing.Common.Constant.TestSetup
+{
+	public class WebDrivers
+	{
+		protected const string BaseLocation = @"C:\Program Files (x86)\WebDrivers";
+		public const string ChromeDriverLocation = BaseLocation + @"\chromedriver.exe";
+	}
+}
