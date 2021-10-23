f84b6c8f372d0043db6fd47a151ded1e5222e791
Mason Hardy <hardymasonj@gmail.com>
2021-10-23 10:55:34 -0500

    Added Facebook test

--- HardyStrategies.Testing.CityLimits/HomeTest.cs
+++ HardyStrategies.Testing.CityLimits/HomeTest.cs
@@ -2,6 +2,7 @@ using HardyStrategies.Testing.CityLimits.SeleniumTests.PageModels;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace HardyStrategies.Testing.CityLimits
 {
@@ -23,6 +24,15 @@ namespace HardyStrategies.Testing.CityLimits
 			var strExpected = "tel:+12189995900";
 			var strActual = this.HomePage.Footer.GetPhoneNumberTarget();
 			Assert.AreEqual(strExpected, strActual);
+		}
+
+		[Test]
+		public void NavigateToFacebookTest()
+		{
+			this.HomePage.Footer.NavigateToFacebook();
+			new WebDriverWait(this.Driver, new System.TimeSpan(0, 0, 30));
+			this.Driver.SwitchTo().Window(this.Driver.WindowHandles[1]);
+			Assert.IsTrue(this.Driver.Url.StartsWith("https://www.facebook.com/CityLimitsStorageCenter/"));
 		}
 
 		[TearDown]
